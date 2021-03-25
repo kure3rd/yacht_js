c6286cf58bbc5a04e590d506ff032b25a528df59
kure3rd <yi1306c12+kure3rd@gmail.com>
2021-03-25 16:09:42 +0900

    add button to change its color

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 function run() {
     console.log("Hello!");
 }
+
+f = 0;
+function yesno(){
+    if(f==0){
+        f=1;
+        console.log('yes');
+        document.getElementById('test').value='yes';
+        document.getElementById('test').style.backgroundColor='#FFFFFF'
+    }
+    else{
+        f=0;
+        console.log('no');
+        document.getElementById('test').value='no';
+        document.getElementById('test').style.backgroundColor='#000000';
+    }
+}
